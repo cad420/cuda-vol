738d1187ad9c3e0ab4d38149522b9dbbca464627
koishi <qwq@koishi.top>
2019-12-24 00:13:01 +0800

    fix conv link

--- tools/ppmconv/ppmconv.cc
+++ tools/ppmconv/ppmconv.cc
@@ -2,9 +2,7 @@
 #include <string>
 #include <cstdlib>
 #include <cxxopts.hpp>
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
 
 using namespace std;
